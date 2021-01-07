e767be4689b271f3ec7c05c96cd62e53c2270473
Jay Arabia <firebender.me@gmail.com>
2021-01-07 09:22:32 +0800

    Add ViewUser controller

---
+++ src/Http/Controllers/ViewUserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace FireBender\Laravel\PictureWorks\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+use FireBender\Laravel\PictureWorks\Services\UserService;
+
+class ViewUserController extends Controller
+{
+    /**
+     * Get paged list of users
+     *
+     * @param FireBender\Laravel\PictureWorks\Services\UserService $service
+     * @return \Illuminate\View\View
+     */
+    public function __invoke(UserService $service, int $id)
+    {
+        $user = $service->getUserById($id);
+
+        $data = ['user' => $user];
+
+        return view('users::view-user', $data);
+    }
+}
