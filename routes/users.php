b9c82bf156a7c5d672f277d007e61dc15e2f3012
Jay Arabia <firebender.me@gmail.com>
2021-01-07 13:41:54 +0800

    Add middleware to view-user route

--- routes/users.php
+++ routes/users.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use FireBender\Laravel\PictureWorks\Http\Controllers\ListUsersController;
 use FireBender\Laravel\PictureWorks\Http\Controllers\ViewUserController;
+use FireBender\Laravel\PictureWorks\Http\Controllers\EditUserController;
+use FireBender\Laravel\PictureWorks\Http\Controllers\ModifyUserController;
 
 // Path: /users
 
@@ -10,4 +12,12 @@ Route::get('users/{page?}', ListUsersController::class)->name('list-users');
 
 // Path: /user/id
 
-Route::get('user/{id}', ViewUserController::class)->name('view-user');
+Route::get('user/{id}', ViewUserController::class)->name('view-user')->middleware('web');
+
+// Path: /user/edit/id
+
+Route::get('user/edit/{id}', EditUserController::class)->name('edit-user')->middleware('web');
+
+// Path: /user/modify
+
+Route::post('user/modify', ModifyUserController::class)->name('modify-user')->middleware('web');
