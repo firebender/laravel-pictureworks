1b7fa483b3211d705e7531af99b271cc5aa8d91b
Jay Arabia <firebender.me@gmail.com>
2021-01-07 09:45:56 +0800

    Add comments to route file

--- routes/users.php
+++ routes/users.php
@@ -4,5 +4,10 @@ use Illuminate\Support\Facades\Route;
 use FireBender\Laravel\PictureWorks\Http\Controllers\ListUsersController;
 use FireBender\Laravel\PictureWorks\Http\Controllers\ViewUserController;
 
+// Path: /users
+
 Route::get('users', ListUsersController::class);
+
+// Path: /users/id
+
 Route::get('users/{id}', ViewUserController::class);
