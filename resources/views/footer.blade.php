d00264ae88d838201b4001bd9f777bf12661e8ca
Jay Arabia <firebender.me@gmail.com>
2021-01-07 12:14:14 +0800

    Open link to PictureWorks in new tab

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,5 @@
     <footer id="footer">
         <ul class="copyright">
-            <li>&copy; <a href="https://pictureworks.com.au/">Pictureworks</a></li>
+            <li>&copy; <a href="https://pictureworks.com.au/" target="_blank">Pictureworks</a></li>
         </ul>
     </footer>
